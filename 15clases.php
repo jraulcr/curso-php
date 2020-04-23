4a51f909f2a7b143115ac63266ca3c2ec7907f87
jraulcr <jraulcr@gmail.com>
2020-04-23 13:00:26 +0200

    ambios de pruebas 23/04/2020

--- 15clases.php
+++ 15clases.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-	<meta charset="utf-8"/>
-	<title>document</title>
-</head>
-<body>
-	<?php 
-	include("vehiculos.php");
-	$mazda=new Coche();
-	$pegaso=new Camion();
-	
-	//echo "el mazda tiene " . $mazda->ruedas . " ruedas <br/>";
-	echo "Pegaso tiene " . $pegaso->getRuedas() . " ruedas. <br/>";
-	//$pegaso->frenar();
-	//$pegaso->arrancar();
+    <head>
+        <meta charset="utf-8"/>
+        <title>document</title>
+    </head>
+    <body>
+        <?php
+        include("vehiculos.php");
+        $mazda = new Coche();
+        $pegaso = new Camion();
 
-	echo " Mazda tiene: " . $mazda->getRuedas() . " ruedas.  <br/>";
-	echo " Mazda tiene: " . $mazda->getMotor(). " cc.  <br/>";
+        //echo "el mazda tiene " . $mazda->ruedas . " ruedas <br/>";
+        echo "Pegaso tiene " . $pegaso->getRuedas() . " ruedas. <br/>";
+        //$pegaso->frenar();
+        //$pegaso->arrancar();
 
-	?>
-</body>
+        echo " Mazda tiene: " . $mazda->getRuedas() . " ruedas.  <br/>";
+        echo " Mazda tiene: " . $mazda->getMotor() . " cc.  <br/>";
+        ?>
+    </body>
 </html>
