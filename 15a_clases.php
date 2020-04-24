1a383e298e38b128187c7fc40f724de89fc67c51
jraulcr <jraulcr@gmail.com>
2020-04-24 17:52:10 +0200

    Continuación curso 24/04/2020

--- 15a_clases.php
+++ 15a_clases.php
@@ -19,7 +19,7 @@
             //Metodo constructor 
             function __construct() { //se le da un valor inicial
                 $this->ruedas = 4;
-                $this->color = "";
+                $this->color = "Gris";
                 $this->motor = 1400;
             }
 
@@ -35,17 +35,67 @@
             function frenar() {
                 echo "Estoy frenando<br>";
             }
-
+            
+            function establece_color($color_coche, $nombre_coche){
+                $this->color=$color_coche;
+                 echo "El color de " .$nombre_coche . " es: " . $this->color . "<br>";       
+            }
         }
 
         //invocacion de objetos o instancia
-        $renault = new Coche();
+       /* $renault = new Coche();
         $mazda = new Coche();
         $seat = new Coche();
+        $renault->establece_color("Rojo", "Renault" );
+        $seat->establece_color("Azul", "Seat" );*/
         
-        $mazda->girar();
+      //  $mazda->girar();
         
-        echo $mazda->ruedas;
+      //  echo $mazda->ruedas;
+        
+      //--------------------------------------------------------------
+        
+        class Camion {
+
+            //propiedades
+            var $ruedas;
+            var $color;
+            var $motor;
+
+            //Metodo constructor 
+            function __construct() { //se le da un valor inicial
+                $this->ruedas = 8;
+                $this->color = "Gris";
+                $this->motor = 2600;
+            }
+
+            //funcionalidades            
+            function arrancar() {
+                echo "Estoy arrancando<br>";
+            }
+
+            function girar() {
+                echo "Estoy girando<br>";
+            }
+
+            function frenar() {
+                echo "Estoy frenando<br>";
+            }
+            
+           /* function establece_color($color_coche, $nombre_coche){
+                $this->color=$color_coche;
+                 echo "El color de " .$nombre_coche . " es: " . $this->color . "<br>";       
+            }*/
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         ?>
     </body>
