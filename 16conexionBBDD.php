9935fb62dd1e41cbc21eecd0f55c1f7bbb1469de
jraulcr <jraulcr@gmail.com>
2020-04-24 19:25:24 +0200

    Continuación curso 24/04/2020

--- 16conexionBBDD.php
+++ 16conexionBBDD.php
@@ -12,23 +12,43 @@ and open the template in the editor.
     <body>
         <?php
         $db_host = "localhost";
-        $db_nombre = "pruebas";
+        $db_nombre = "pruebas02";
         $db_usuario = "root";
         $db_contra = "";
 
-        $conexion = mysqli_connect($db_host, $db_usuario, $db_contra, $db_nombre);
+        //$conexion = mysqli_connect($db_host, $db_usuario, $db_contra, $db_nombre);
+        $conexion = mysqli_connect($db_host, $db_usuario, $db_contra);
+        
+        //Si ha fallado la conexion con la BB.DD. p.e.: en vez de 'localhost' se escribe 'localhossst'
+        if(mysqli_connect_errno()){
+            echo "<br>***Fallo al conectar la BB.DD***<br>";
+            //Sale del codigo PHP
+            exit();
+        }
 
+        //Especificar la base de datos en que queremos conectarnos
+        mysqli_select_db($conexion, $db_nombre) or die ('No se encuentra la BB.DD');
+        
+        //Utilizar los caracteres latinos
+        mysqli_set_charset($conexion, "utf8");         
+      
+         //Query
         $consulta = "SELECT * FROM DATOSPERSONALES";
-
-        $resultados = mysqli_query($conexion, $consulta);
         //Resultset
+        $resultados = mysqli_query($conexion, $consulta);        
 
-        $fila = mysqli_fetch_row($resultados);
-
+        while ($fila= mysqli_fetch_row($resultados)){      
+            
         echo $fila[0] . " ";
         echo $fila[1] . " ";
         echo $fila[2] . " ";
         echo $fila[3] . " ";
+        echo "<br> ";       
+        
+        }
+        
+        mysqli_close($conexion);
+        
         ?>
     </body>
 </html>
