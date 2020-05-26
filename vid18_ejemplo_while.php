b63cb8c0f33907b67758988c0662ebcb5cc5376f
jraulcr <jraulcr@gmail.com>
2020-05-26 10:49:59 +0200

    Cambios curso PHP 26/05/2020

---
+++ vid18_ejemplo_while.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+        $var1 = 1;
+
+        do {
+
+            echo "Estamos ejecutando el código del bucle<br/>";
+
+            $var1++;
+            
+        } while ($var1 < 6);
+
+        echo "Hemos salido del bucle";
+
+
+        /*
+          $var1 = 1;
+
+          while ($var1 < 6) {
+
+          echo "Estamos ejecutando el código del bucle<br/>";
+
+          $var1++;
+
+          }
+
+          echo "Hemos salido del bucle";
+         */
+        ?>
+
+
+    </body>
+</html>
