e52fc1d36011e9219ffd6424bc0a2da9e360515d
jraulcr <jraulcr@gmail.com>
2020-06-28 18:57:56 +0200

    Curso PHP Pildorasinformaticas 25/06/2020

--- MVC/controlador/Productos_controlador.php
+++ MVC/controlador/Productos_controlador.php
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
 <?php
 
 
     require_once("modelo/Productos_modelo.php");
-    
-    $productos=new Productos_modelo();    
+
+    $productos=new Productos_modelo();
     $matrizProductos=$productos->getProductos();
-    
+
     require_once ("vista/Productos_view.php");
 
 
-=======
-<?php
-
-
-    require_once("modelo/Productos_modelo.php");
-    
-    $productos=new Productos_modelo();    
-    $matrizProductos=$productos->getProductos();
-    
-    require_once ("vista/Productos_view.php");
-
-
->>>>>>> c1b97b056abcdf8ef6a032c122be8adfbd9f1c80
 ?>
--- MVC/modelo/Conectar.php
+++ MVC/modelo/Conectar.php
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-class Conectar {
-
-    public static function conexion() {
-
-        try {
-            $conexion = new PDO('mysql:host=localhost; port=3306; dbname=pruebas', 'root', '');
-            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $conexion->exec("SET CHARACTER SET UTF8");
-        } catch (Exception $e) {
-            die("Error: " . $e->getMessage());
-            echo $e->getLine();
-        }
-
-        return $conexion;
-    }
-}
-=======
 <?php
 
 /*
@@ -48,4 +28,3 @@ class Conectar {
     }
 
 }
->>>>>>> c1b97b056abcdf8ef6a032c122be8adfbd9f1c80
